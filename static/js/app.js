273691a702afeebe214a174445095265ee6b324c
AhldenBrough <50213731+AhldenBrough@users.noreply.github.com>
2023-12-22 17:40:15 -0800

    added gauge

--- static/js/app.js
+++ static/js/app.js
@@ -25,13 +25,14 @@ function createAll(name) {
     let currentBelly = data.samples.find(sample => sample.id === name);
     let currentMetadata = data.metadata.find(sample => sample.id === parseInt(name));
 
-    createBar(name, currentBelly);
-    createBubble(name, currentBelly);
-    createDemographicInfo(name, currentMetadata);
+    createBar(currentBelly);
+    createBubble(currentBelly);
+    createDemographicInfo(currentMetadata);
+    createGauge(currentMetadata);
   });
 }
 
-function createBar(name, currentBelly){
+function createBar(currentBelly){
     
     let trace = {
       x: currentBelly.sample_values.slice(0,10).reverse(),
@@ -49,7 +50,7 @@ function createBar(name, currentBelly){
 
 }
 
-function createBubble(name, currentBelly){
+function createBubble(currentBelly){
     
     let trace = {
       x: currentBelly.otu_ids,
@@ -72,15 +73,52 @@ function createBubble(name, currentBelly){
 
 }
 
-function createDemographicInfo(name, currentMetadata){
-  let ul = d3.select('#sample-metadata');
+function createDemographicInfo(currentMetadata){
+  let info = d3.select('#sample-metadata');
   console.log(currentMetadata);
 
   for (let key in currentMetadata) {
     if (currentMetadata.hasOwnProperty(key)) {
-      ul.append('p').text(`${key}: ${currentMetadata[key]}\n`);
+      info.append('p').text(`${key}: ${currentMetadata[key]}\n`);
     }
   }
+}
+
+function createGauge(currentMetadata){
+  let trace = {
+    type: "indicator",
+    mode: "gauge+number",
+    title: {
+      text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week",
+      font: {color: "black", size: 16}
+    },
+    value: currentMetadata.wfreq,
+    gauge: {
+      axis: { range: [0, 9], tickwidth: 1, tickcolor: "darkblue" },
+      bar: { color: "red" },
+      steps: [
+        { range: [0, 1], color: "rgba(255, 0, 0, 0.7)", label: "0-1", text: "Step 1" },
+        { range: [1, 2], color: "rgba(255, 165, 0, 0.7)", label: "1-2", text: "Step 2" },
+        { range: [2, 3], color: "rgba(255, 255, 0, 0.7)", label: "2-3", text: "Step 3" },
+        { range: [3, 4], color: "rgba(0, 128, 0, 0.7)", label: "3-4", text: "Step 4" },
+        { range: [4, 5], color: "rgba(0, 0, 255, 0.7)", label: "4-5", text: "Step 5" },
+        { range: [5, 6], color: "rgba(75, 0, 130, 0.7)", label: "5-6", text: "Step 6" },
+        { range: [6, 7], color: "rgba(238, 130, 238, 0.7)", label: "6-7", text: "Step 7" },
+        { range: [7, 8], color: "rgba(255, 20, 147, 0.7)", label: "7-8", text: "Step 8" },
+        { range: [8, 9], color: "rgba(173, 216, 230, 0.7)", label: "8-9", text: "Step 9" },
+      ],
+    },
+    
+    threshold: {
+      line: { color: "red", width: 8 },
+      thickness: 0.75,
+      value: currentMetadata.wfreq
+    }
+  }
+
+  let s = [trace];
+
+    Plotly.newPlot("gauge", s);
 }
 
 createAll(start_id);
